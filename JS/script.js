d393fd276a2e08d006032eb8729f9b3a3d7bf447
Connor Allen <connor27allen@gmail.com>
2024-02-21 19:07:12 -0500

    final

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,8 @@ var searchInput = $('#search-input');
 var searchBtn = $('#search-btn');
 var apiKey = 'e9dd823f996631a8acc598c7c9e0c07d';
 var currentUrl = 'https://api.openweathermap.org/data/2.5/weather?appid=e9dd823f996631a8acc598c7c9e0c07d&units=imperial&';
-var forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=atlanta&appid=e9dd823f996631a8acc598c7c9e0c07d&units=imperial&';
+var forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?appid=e9dd823f996631a8acc598c7c9e0c07d&units=imperial&';
+var historyList = $('#search-history');
 
 function getSearchHistory() {
   var rawData = localStorage.getItem('search-history');
@@ -10,8 +11,11 @@ function getSearchHistory() {
   return history;
 }
 
+function saveSearchHistory(history) {
+  localStorage.setItem('search-history', JSON.stringify(history));
+}
+
 function displayCurrentWeather(data) {
-  
   var cityName = data.name;
   var date = new Date(data.dt * 1000).toLocaleDateString();
   var icon = data.weather[0].icon;
@@ -19,7 +23,6 @@ function displayCurrentWeather(data) {
   var humidity = data.main.humidity;
   var windSpeed = data.wind.speed;
 
-  
   $('#city-name').text(cityName);
   $('#date').text(date);
   $('#weather-icon').attr('src', `http://openweathermap.org/img/w/${icon}.png`);
@@ -29,77 +32,82 @@ function displayCurrentWeather(data) {
 }
 
 function displayForecast(data) {
-  
   var forecast = data.list;
 
-  
   for (var i = 0; i < forecast.length; i++) {
+    // var forecastObj = forecast[i * 8];
     var forecastObj = forecast[i];
-    var date = new Date(forecastObj.dt * 1000).toLocaleDateString();
-    var icon = forecastObj.weather[i].icon;
-    var temperature = forecastObj.main.temp;
-    var humidity = forecastObj.main.humidity;
-    var windSpeed = forecastObj.wind.speed;
+    var time = forecastObj.dt_txt;
 
-    
-    $('#forecast-date-' + i).text(date);
-    $('#forecast-icon-' + i).attr('src', `http://openweathermap.org/img/w/${icon}.png`);
-    $('#forecast-temperature-' + i).text(temperature);
-    $('#forecast-humidity-' + i).text(humidity);
-    $('#forecast-wind-speed-' + i).text(windSpeed);
+    if (time.includes('12:00')) {
+      var date = new Date(forecastObj.dt * 1000).toLocaleDateString();
+      var icon = forecastObj.weather[0].icon;
+      var temperature = forecastObj.main.temp;
+      var humidity = forecastObj.main.humidity;
+      var windSpeed = forecastObj.wind.speed;
+
+      $('#forecast-output').append(`
+      <div>
+          <p>${date}</p>
+          <img src="${`http://openweathermap.org/img/w/${icon}.png`}" alt="Weather Icon">
+          <p>Temp: ${temperature}</p>
+          <p>Humidity: ${humidity}</p>
+          <p>Wind: ${windSpeed} mph</p>
+      </div>
+      
+      `);
+      
+    }
+
+    // forecastContainer.append(date, icon, temperature, humidity, windSpeed);
+
+    // Append forecast container to the page
+    // $('#forecast-container').append(forecastContainer);
   }
 }
 
-function getCurrentForecast() {
-  var cityName = searchInput.val();
+function getCurrentForecast(cityName) {
   var history = getSearchHistory();
 
   if (!history.includes(cityName)) {
     history.push(cityName);
-    localStorage.setItem('search-history', JSON.stringify(history));
+    saveSearchHistory(history);
   }
 
   $.get(currentUrl + `q=${cityName}`)
-    .then(function(data) {
+    .then(function (data) {
       displayCurrentWeather(data);
     });
 
   $.get(forecastUrl + `q=${cityName}`)
-    .then(function(data) {
+    .then(function (data) {
       displayForecast(data);
     });
 }
 
-function renderSearchHistory(history) {
-  
-  var historyList = $('#search-history');
-
-  
+function renderSearchHistory() {
+  var history = getSearchHistory();
   historyList.empty();
 
-  
   for (var i = 0; i < history.length; i++) {
     var city = history[i];
+    var buttonItem = $('<button>').text(city);
 
-    
-    var listItem = $('<li>').text(city);
-
-    
-    listItem.on('click', function() {
+    buttonItem.on('click', function () {
       var clickedCity = $(this).text();
       getCurrentForecast(clickedCity);
     });
 
-    
-    historyList.append(listItem);
+    historyList.append(buttonItem);
   }
 }
 
-searchBtn.on('click', function(event) {
+searchBtn.on('click', function (event) {
   event.preventDefault();
-  getCurrentForecast();
+  var cityName = searchInput.val();
+  getCurrentForecast(cityName);
+  renderSearchHistory();
 });
 
-
-var history = getSearchHistory();
-renderSearchHistory(history);
+// Initial rendering of search history
+renderSearchHistory();
